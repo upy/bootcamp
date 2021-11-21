e9e2bcac952de30b3c68a94cc54fc482a636df14
upy <caner@cakici.net>
2021-11-21 14:55:25 +0300

    added django extensions

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "django.contrib.sites",
+    "django_extensions"
 ]
 
 MIDDLEWARE = [
