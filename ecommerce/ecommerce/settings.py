06be6b4239869f82d3d5c570ca96a5e4f7948d31
VeliAliyev <veli_aliyev@outlook.com>
2021-11-25 18:10:16 +0300

    function to find path to .env file is added to settings.py

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -12,13 +12,24 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import environ
+import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# Set the project base directory
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Initialise environment variables
+
+def find_path(filename):
+    for dirpath, dirs, files in os.walk(BASE_DIR):
+        for name in files:
+            if name == filename:
+                return os.path.abspath(os.path.join(dirpath, name))
+
+# Find path to .env file
+ENV_FILE = find_path('.env')
+
+# Take environment variables from .env file
+environ.Env.read_env(ENV_FILE)
 env = environ.Env()
-environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
