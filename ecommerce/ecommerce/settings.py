a20ce18785d0c2b8657a91eba8b7cdeb39ed36c0
Alper Mehmet Özdemir <32219367+AlperMehmetOzdemir@users.noreply.github.com>
2021-11-25 07:46:01 +0300

    Update settings.py

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -29,7 +29,6 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# 2436135
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
 
