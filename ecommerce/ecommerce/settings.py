76e065704dae260720ac285f678f4ca0bc3569fe
upy <caner@cakici.net>
2021-11-28 12:42:30 +0300

    added customer model

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "django_extensions"
+    "django_extensions",
+    "customers",
 ]
 
 MIDDLEWARE = [
@@ -134,3 +135,4 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = "customers.Customer"
