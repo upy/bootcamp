ad887237caf06f6cd70f87b7488aa4d0b0feef27
upy <caner@cakici.net>
2021-11-28 13:31:29 +0300

    added core app

---
+++ ecommerce/core/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class BaseAbstractModel(models.Model):
+    created_at = models.DateTimeField(verbose_name=_("Created at"), auto_now_add=True)
+    modified_at = models.DateTimeField(verbose_name=_("Modified at"), auto_now=True)
+
+    class Meta:
+        abstract = True
