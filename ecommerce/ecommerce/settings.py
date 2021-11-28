4f4a43ea45cc8bf8e1281aff3bf5bd5ea97aa7ed
upy <caner@cakici.net>
2021-11-28 13:31:53 +0300

    added products app

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -43,7 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "django_extensions",
+    "core",
     "customers",
+    "products",
 ]
 
 MIDDLEWARE = [
---
+++ ecommerce/products/enums.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Colors(models.TextChoices):
+    RED = "red", _("Red")
+    BLUE = "blue", _("Blue")
+    WHITE = "white", _("White")
+    YELLOW = "yellow", _("Yellow")
