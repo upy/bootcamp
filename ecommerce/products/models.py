fa238d77a68bafb4dbb291b66cead851adebe7d1
Alper Mehmet Özdemir <amehmet97@hotmail.com>
2021-11-29 15:46:27 +0300

    Added category model to products app. Added M2M relation to product model to categories. Applied associated migrations.

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -5,10 +5,21 @@ from core.models import BaseAbstractModel
 from products import enums
 
 
+class Category(BaseAbstractModel):
+    name = models.CharField(verbose_name=_("Name"), max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
 class Product(BaseAbstractModel):
     sku = models.CharField(verbose_name=_("SKU"), max_length=100, unique=True)
     name = models.CharField(max_length=255, verbose_name=_("Name"))
     description = models.TextField(max_length=2000, verbose_name=_("Description"))
+    categories = models.ManyToManyField(Category)
     color = models.CharField(
         choices=enums.Colors.choices, verbose_name=_("Color"), max_length=20)
     size = models.CharField(max_length=30, verbose_name=_("Size"))
