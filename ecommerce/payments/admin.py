7b001418f35b9a98a239d27511dba0efca302cf7
VeliAliyev <veli_aliyev@outlook.com>
2021-12-01 19:40:36 +0300

    payments app created with bank and bank account models

---
+++ ecommerce/payments/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from payments.models import Bank, BankAccount
+
+
+@admin.register(Bank)
+class BankAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+@admin.register(BankAccount)
+class BankAccountAdmin(admin.ModelAdmin):
+    list_display = ("bank", "name", "iban")
