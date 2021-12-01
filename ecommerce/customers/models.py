290b572739bd3c0524e2f50ff02b8debd4b616d3
Emincan YAZGI <ecanyazgi@gmail.com>
2021-12-02 00:16:52 +0300

    Products Customer Models Completed

--- ecommerce/customers/models.py
+++ ecommerce/customers/models.py
@@ -4,10 +4,10 @@ from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.core.mail import send_mail
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
-
 from django.db import models
-
 from customers.managers import CustomerManager
+from core.models import BaseAbstractModel
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Customer(AbstractBaseUser, PermissionsMixin):
@@ -67,3 +67,49 @@ class Customer(AbstractBaseUser, PermissionsMixin):
     def email_user(self, subject, message, from_email=None, **kwargs):
         """Send an email to this user."""
         send_mail(subject, message, from_email, [self.email], **kwargs)
+
+
+class Country(BaseAbstractModel):
+    name = models.CharField(max_length=255, verbose_name=_('Country'))
+
+    class Meta:
+        verbose_name = _('country')
+        verbose_name_plural = _('countries')
+
+    def __str__(self):
+        return self.name
+
+
+class City(BaseAbstractModel):
+    name = models.CharField(max_length=255, verbose_name=_('City'))
+    country = models.ForeignKey(Country, verbose_name=_('Country'),
+                                on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = _('city')
+        verbose_name_plural = _('cities')
+
+    def __str__(self):
+        return self.name
+
+
+class Address(BaseAbstractModel):
+    name = models.CharField(max_length=255, verbose_name=_('Name'))
+    full_name = models.CharField(max_length=255, verbose_name=_('Full Name'))
+    line_1 = models.CharField(max_length=255, verbose_name=_('First Address Line'))
+    line_2 = models.CharField(max_length=255, verbose_name=_('Second Address Line'),
+                              help_text=_('Not Mandatory'))
+    phone = PhoneNumberField()
+    district = models.CharField(max_length=255,verbose_name=_("District"))
+    postcode = models.CharField(max_length=255,verbose_name=_('Post Code'))
+    city = models.ForeignKey(City,verbose_name=_('City'),on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = _("address")
+        verbose_name_plural = _("addresses")
+
+    def __str__(self):
+        return self.name
+
+
+
--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "django_extensions",
+    "phonenumber_field",
     "core",
     "customers",
     "products",
