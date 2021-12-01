180ef12ec832edbf8dbec7265eda2cdbae6634cf
VeliAliyev <veli_aliyev@outlook.com>
2021-12-01 17:53:51 +0300

    category model and admin added

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -5,6 +5,17 @@ from core.models import BaseAbstractModel
 from products import enums
 
 
+class Category(BaseAbstractModel):
+    name = models.CharField(max_length=30, verbose_name=_("Name"))
+
+    class Meta:
+        verbose_name = _("category")
+        verbose_name_plural = _("categories")
+
+    def __str__(self):
+        return f"{self.name}"
+
+
 class Product(BaseAbstractModel):
     sku = models.CharField(verbose_name=_("SKU"), max_length=100, unique=True)
     name = models.CharField(max_length=255, verbose_name=_("Name"))
@@ -12,6 +23,10 @@ class Product(BaseAbstractModel):
     color = models.CharField(
         choices=enums.Colors.choices, verbose_name=_("Color"), max_length=20)
     size = models.CharField(max_length=30, verbose_name=_("Size"))
+    categories = models.ManyToManyField(Category, verbose_name=_("Category"))
+
+    """def category(self):
+        return "\n".join([c.name for c in self.categories.all()])"""
 
     class Meta:
         verbose_name = _("product")
@@ -46,4 +61,5 @@ class Price(BaseAbstractModel):
 
     def __str__(self):
         return f"{self.product} - {self.amount}"
+
 
