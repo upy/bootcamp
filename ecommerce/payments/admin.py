91cae296bb4997756675c3d94ca45a7e1c2c9c62
Toygar Aksoy <toygar.aksoy@gmail.com>
2021-12-03 00:57:37 +0300

    payments app done

--- ecommerce/payments/admin.py
+++ ecommerce/payments/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from payments.models import Bank, BankAccount
+
+
+@admin.register(Bank)
+class BankAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_display = ("name",)
+
+
+@admin.register(BankAccount)
+class BankAccountAdmin(admin.ModelAdmin):
+    search_fields = ("bank", "name", "iban")
+    list_display = ("bank", "name", "iban")
