99e9efd42c8e2f95ac8a899027b723cc15b2a40a
Toygar Aksoy <toygar.aksoy@gmail.com>
2021-12-02 18:37:30 +0300

    baskets app completed

--- ecommerce/baskets/admin.py
+++ ecommerce/baskets/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from baskets.models import Basket, BasketItem
+
+
+@admin.register(Basket)
+class BasketAdmin(admin.ModelAdmin):
+    search_fields = ("customer", )
+    list_filter = ("status", )
+    list_display = ("customer", "status")
+
+
+@admin.register(BasketItem)
+class BasketItemAdmin(admin.ModelAdmin):
+    list_display = ("product", "quantity", "price")
--- ecommerce/baskets/models.py
+++ ecommerce/baskets/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from core.models import BaseAbstractModel
 
-# Create your models here.
+from customers.models import Customer
+from products.models import Product
+from baskets import enums
+
+
+class Basket(BaseAbstractModel):
+    customer = models.ForeignKey(Customer,
+                                 verbose_name=_("Customer"),
+                                 on_delete=models.CASCADE,
+                                 null=True)
+    status = models.CharField(max_length=50,
+                              verbose_name=_("Status"),
+                              choices=enums.Status.choices
+                              )
+
+    class Meta:
+        verbose_name = _("Basket")
+        verbose_name_plural = _("Baskets")
+
+    def __str__(self):
+        return f"{self.customer} : {self.status}"
+
+
+class BasketItem(BaseAbstractModel):
+    basket = models.ForeignKey(Basket,
+                               verbose_name=_("Basket"),
+                               on_delete=models.CASCADE,
+                               null=True)
+    product = models.ForeignKey(Product,
+                                verbose_name=_("Product"),
+                                on_delete=models.CASCADE,
+                                null=True)
+    quantity = models.PositiveSmallIntegerField(default=0,
+                                                verbose_name=_("Quantity"))
+    price = models.DecimalField(default=0,
+                                max_digits=10,
+                                decimal_places=2,
+                                verbose_name=_("Price"))
+
+    class Meta:
+        verbose_name = _("Item")
+        verbose_name_plural = _("Items")
+
+    def __str__(self):
+        return f"{self.product} : {self.quantity} : {self.price}"
