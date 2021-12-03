6761ea926a1ba9940d8b3eb4af49a02dc2ae291a
ogzhncvdr <oguzhancavdar06@gmail.com>
2021-12-04 02:33:21 +0300

    apply regex and base address abstract

--- ecommerce/baskets/models.py
+++ ecommerce/baskets/models.py
@@ -10,18 +10,22 @@ class Basket(BaseAbstractModel):
     customer = models.ForeignKey(Customer, verbose_name=_('Customer'), null=True, on_delete=models.CASCADE)
     status = models.CharField(choices=Status.choices, verbose_name=_('Status'), max_length=20)
 
+    @property
+    def get_status_display(self):
+        return f"{self.customer}-{self.status}"
+
     class Meta:
         verbose_name = _("basket")
         verbose_name_plural = _("baskets")
 
     def __str__(self):
-        return f"{self.customer} - {self.status}"
+        return f"{self.get_status_display}"
 
 
 class BasketItem(BaseAbstractModel):
     basket = models.ForeignKey(Basket, verbose_name=_('Basket'), on_delete=models.CASCADE)
     product = models.ForeignKey(Product, verbose_name=_('Product'), on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(verbose_name=_('Quantity'),)
+    quantity = models.PositiveIntegerField(verbose_name=_('Quantity'), )
     price = models.DecimalField(verbose_name=_('Price'), max_digits=10, decimal_places=2)
 
     class Meta:
--- ecommerce/payments/models.py
+++ ecommerce/payments/models.py
@@ -1,9 +1,7 @@
 from django.db import models
 from core.models import BaseAbstractModel
 from django.utils.translation import gettext_lazy as _
-from django.core.validators import RegexValidator
-
-IBAN_REGEX = "^[a-zA-Z]{2}[0-9]{2}\s?[a-zA-Z0-9]{4}\s?[0-9]{4}\s?[0-9]{3}([a-zA-Z0-9]\s?[a-zA-Z0-9]{0,4}\s?[a-zA-Z0-9]{0,4}\s?[a-zA-Z0-9]{0,4}\s?[a-zA-Z0-9]{0,3})?$"
+from core.regex_validators import IBAN_REGEX
 
 
 class Bank(BaseAbstractModel):
@@ -20,8 +18,7 @@ class Bank(BaseAbstractModel):
 class BankAccount(BaseAbstractModel):
     bank = models.ForeignKey(Bank, verbose_name=_('Bank'), on_delete=models.CASCADE)
     name = models.CharField(max_length=255, verbose_name=_('Name'))
-    iban_regex = RegexValidator(regex=IBAN_REGEX, message="Your IBAN number is invalid.")
-    iban = models.CharField(validators=[iban_regex], max_length=34, verbose_name=_('IBAN'), unique=True)
+    iban = models.CharField(validators=[IBAN_REGEX], max_length=34, verbose_name=_('IBAN'), unique=True)
 
     class Meta:
         verbose_name = _("bank account")
