6118f804f699376e97dafcad091f86c12cfcb548
Alper Mehmet Özdemir <amehmet97@hotmail.com>
2021-12-03 20:41:30 +0300

    Product app, admin setup

--- ecommerce/products/admin.py
+++ ecommerce/products/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from products.models import Product, Stock, Price
+from products.models import Product, Stock, Price, Category
 
 
 class StockInline(admin.StackedInline):
@@ -11,22 +11,35 @@ class PriceInline(admin.StackedInline):
     model = Price
 
 
+class CategoryInline(admin.StackedInline):
+    model = Category
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     search_fields = ("name", "sku")
-    list_display = ("sku", "name", "color", "size")
+    list_display = ("sku", "name", "color", "size", "get_categories")
     inlines = [StockInline, PriceInline]
+
+    def get_categories(self, obj):
+        return "\n".join([p.name for p in obj.categories.all()])
 
 
 @admin.register(Stock)
 class StockAdmin(admin.ModelAdmin):
     list_display = ("product", "quantity")
     search_fields = ("product__name", "product__sku")
-    autocomplete_fields = ("product", )
+    autocomplete_fields = ("product",)
 
 
 @admin.register(Price)
 class StockAdmin(admin.ModelAdmin):
     list_display = ("product", "amount")
     search_fields = ("product__name", "product__sku")
-    autocomplete_fields = ("product", )
+    autocomplete_fields = ("product",)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_display = ("name",)
