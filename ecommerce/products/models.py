dfc86450abd443f52fc02c8c0738576620f1ccf5
eylultuncel <eylultuncel1999@gmail.com>
2021-12-03 17:17:27 +0300

    product m2m relation added

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -5,6 +5,17 @@ from core.models import BaseAbstractModel
 from products import enums
 
 
+class Category(BaseAbstractModel):
+    name = models.CharField(max_length=255, verbose_name=_("Name"))
+
+    class Meta:
+        verbose_name = _("category")
+        verbose_name_plural = _("categories")
+
+    def __str__(self):
+        return f"{self.name}"
+
+
 class Product(BaseAbstractModel):
     sku = models.CharField(verbose_name=_("SKU"), max_length=100, unique=True)
     name = models.CharField(max_length=255, verbose_name=_("Name"))
@@ -12,6 +23,7 @@ class Product(BaseAbstractModel):
     color = models.CharField(
         choices=enums.Colors.choices, verbose_name=_("Color"), max_length=20)
     size = models.CharField(max_length=30, verbose_name=_("Size"))
+    categories = models.ManyToManyField(Category)
 
     class Meta:
         verbose_name = _("product")
@@ -47,14 +59,4 @@ class Price(BaseAbstractModel):
     def __str__(self):
         return f"{self.product} - {self.amount}"
 
-
-class Category(BaseAbstractModel):
-    name = models.CharField(max_length=255, verbose_name=_("Name"))
-
-    class Meta:
-        verbose_name = _("category")
-        verbose_name_plural = _("categories")
-
-    def __str__(self):
-        return f"{self.name}"
 
