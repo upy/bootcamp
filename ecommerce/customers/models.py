9da5e7ad3521b967f7418335010a7cbd761e6cd8
DESKTOP-K3INLGF\aysenur <aysenur_yzc@icloud.com>
2021-12-03 17:19:14 +0300

    new models added to Customers app

--- ecommerce/customers/models.py
+++ ecommerce/customers/models.py
@@ -8,6 +8,10 @@ from django.utils.translation import gettext_lazy as _
 from django.db import models
 
 from customers.managers import CustomerManager
+from core.models import BaseAbstractModel
+from django.core.validators import RegexValidator
+
+NUM_REGEX = RegexValidator('^[0-9]*$')
 
 
 class Customer(AbstractBaseUser, PermissionsMixin):
@@ -67,3 +71,44 @@ class Customer(AbstractBaseUser, PermissionsMixin):
     def email_user(self, subject, message, from_email=None, **kwargs):
         """Send an email to this user."""
         send_mail(subject, message, from_email, [self.email], **kwargs)
+
+
+class Country(BaseAbstractModel):
+    name = models.CharField(max_length=255, verbose_name=_("Country"))
+
+    class Meta:
+        verbose_name = _("county")
+        verbose_name_plural = _("countries")
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class City(BaseAbstractModel):
+    name = models.CharField(max_length=255, verbose_name=_("City"))
+    country = models.ForeignKey(Country, verbose_name=_("Country"), on_delete=models.PROTECT)
+
+    class Meta:
+        verbose_name = _("city")
+        verbose_name_plural = _("cities")
+
+    def __str__(self):
+        return f"{self.name} - {self.country}"
+
+
+class Address(BaseAbstractModel):
+    name = models.CharField(max_length=255, verbose_name=_("Address Name"))
+    full_name = models.CharField(max_length=255, verbose_name=_("Full Name"))
+    line_1 = models.CharField(max_length=255, verbose_name=_("Line 1"))
+    line_2 = models.CharField(max_length=255, verbose_name=_("Line 2"))
+    phone = models.CharField(max_length=15, verbose_name=_("Phone"), validators=[NUM_REGEX])
+    district = models.CharField(max_length=20, verbose_name=_("District"))
+    postcode = models.CharField(max_length=10, verbose_name=_("Postcode"), validators=[NUM_REGEX])
+    city = models.ForeignKey(City, verbose_name=_("City"), on_delete=models.PROTECT)
+
+    class Meta:
+        verbose_name = _("address")
+        verbose_name_plural = _("addresses")
+
+    def __str__(self):
+        return f"{self.name} - {self.line_1}, {self.line_2} - {self.district}/{self.city} - {self.postcode} - {self.phone}"
