7148956c50b4dbe089047cab3b1ac76421014042
smlblr <samilblr@gmail.com>
2021-12-04 03:22:46 +0300

    Added address model in customers app

--- ecommerce/customers/admin.py
+++ ecommerce/customers/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import gettext_lazy as _
-from customers.models import Customer
+from customers.models import Customer, Address, City, Country
+
+
+class CityInline(admin.TabularInline):
+    model = City
+
+
+class CountryInline(admin.TabularInline):
+    model = Country
 
 
 @admin.register(Customer)
@@ -36,3 +44,26 @@ class CustomerAdmin(UserAdmin):
     list_display = ("email", "first_name", "last_name", "is_staff")
     search_fields = ("first_name", "last_name", "email")
     ordering = ("email",)
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ("customer", "name", "city")
+    list_filter = ("city",)
+    search_fields = ("line_1", "line_2", "city")
+    # inlines = (CityInline, CountryInline) #TODO: Add inlines
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ("name", "country")
+    list_filter = ("country",)
+    search_fields = ("city",)
+    # inlines = (CountryInline,) #TODO: Add inlines
+
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    list_filter = ("name",)
+    search_fields = ("name",)
