fd613c47fd2aaf1c3842498e12cc825e5627be48
smlblr <samilblr@gmail.com>
2021-12-04 03:29:12 +0300

    Created phone number and iban validator with regex and stored these in core app as utils

---
+++ ecommerce/core/utils.py
@@ -0,0 +1,24 @@
+from django.core.validators import RegexValidator
+from django.utils.translation import gettext_lazy as _
+
+
+class PhoneNumberValidator(RegexValidator):
+    regex = r'^\+[0-9]{1,3}\d{10}$'
+    message = _(
+        'Phone number must be entered in the format: +901234567890. '
+        'Up to 13 digits allowed.'
+    )
+    flags = 0
+
+
+class IBANValidator(RegexValidator):
+    """
+    IBAN validator.
+    """
+    regex = r'^[A-Z]{2}[0-9]{2}[A-Z0-9]{4}[0-9]{7}([A-Z0-9]?){0,16}$'
+    message = _('Enter a valid IBAN number.')
+    flags = 0
+
+
+phonenumber_validator = PhoneNumberValidator()
+iban_validator = IBANValidator()
