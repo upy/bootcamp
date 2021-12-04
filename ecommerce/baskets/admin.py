197f42d341e5fe20acbfed4c2d0aa08e835004bc
Alper Mehmet Özdemir <amehmet97@hotmail.com>
2021-12-04 10:56:23 +0300

    Configured baskets app admin.

--- ecommerce/baskets/admin.py
+++ ecommerce/baskets/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from baskets.models import Basket, BasketItem
+
+
+@admin.register(Basket)
+class BasketAdmin(admin.ModelAdmin):
+    search_fields = ("customer__name",)
+    list_display = ("customer", "status")
+    list_filter = ("status",)
+
+
+@admin.register(BasketItem)
+class BasketItemAdmin(admin.ModelAdmin):
+    search_fields = ("product__name", "basket", "product__name")
+    list_display = ("product", "basket", "quantity", "price")
