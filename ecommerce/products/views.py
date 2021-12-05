11e850034f44459d85c1a7dcb5bae088e17ee2a9
upy <caner@cakici.net>
2021-12-05 22:47:01 +0300

    added product and category serializer and viewsets.

--- ecommerce/products/views.py
+++ ecommerce/products/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from products.filters import ProductFilter
+from products.models import Product, Category
+from products.serializers import ProductSerializer, CategorySerializer, \
+    ProductDetailedSerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filterset_class = ProductFilter
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class ProductDetailedViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.prefetch_related("categories").all()
+    serializer_class = ProductDetailedSerializer
+    http_method_names = ["get"]
+
