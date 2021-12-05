a1586b781c8c21dd98419e13c502a4a9c1162aa4
upy <caner@cakici.net>
2021-12-05 22:50:33 +0300

    remove pseudo example

--- ecommerce/orders/forms.py
+++ ecommerce/orders/forms.py
@@ -12,12 +12,6 @@ class OrderAdminForm(forms.ModelForm):
 
     def clean(self):
         cleaned_data = super().clean()
-        ship = cleaned_data.get("shipping_address")
-        customer = cleaned_data.get("customer")
-        if not customer.address_set.filter(id=ship.id).exists():
-            raise ValidationError(
-                message={"shipping_address": _("Shipping address must be customer address")})
-
         if cleaned_data.get("total_price") and cleaned_data.get("total_price") < 10:
             raise ValidationError(message={"total_price": _("Total must be greater than 10")})
         return cleaned_data
