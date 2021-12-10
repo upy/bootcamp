5aa1dfa6d67d540ff226a74df8166238a1aa9273
Ertuğrul BAL <ertgrl.bal@gmail.com>
2021-12-10 15:39:44 +0300

    Basket app models' serializers was created and checked at localhos/api

--- ecommerce/baskets/views.py
+++ ecommerce/baskets/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from core.mixins import DetailedViewSetMixin
+from baskets.filters import BasketFilter
+from baskets.models import Basket, BasketItem
+from baskets.serializers import BasketSerializer, BasketItemSerializer, BasketItemDetailedSerializer
+
+class BasketViewSet(viewsets.ModelViewSet):
+    queryset = Basket.objects.all()
+    serializer_class = BasketSerializer
+    filterset_class = BasketFilter
+
+class BasketItemViewSet(viewsets.ModelViewSet):
+    queryset = BasketItem.objects.all()
+    serializer_class = BasketItemSerializer
+
+class BasketItemDetailedViewSet(viewsets.ModelViewSet):
+    queryset = BasketItem.objects.all()
+    serializer_class = BasketItemDetailedSerializer
+    http_method_names = ["get"]
--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -19,18 +19,31 @@ from django.urls import path, include
 from ecommerce.router import router
 from products.views import ProductViewSet, CategoryViewSet, ProductDetailedViewSet
 from customers.views import CustomerViewSet, AddressViewSet, AddressDetailedViewSet, CityViewSet, CountryViewSet
-
+from baskets.views import BasketViewSet, BasketItemViewSet, BasketItemDetailedViewSet
+"""
+    ## these routers was designed for showing all details about Product and Product's Category Details
+"""
 # Product Routers
 router.register("products", ProductViewSet)
 router.register("products-detailed", ProductDetailedViewSet, basename="product-detailed")
 router.register("categories", CategoryViewSet)
-
+"""
+    ## these routers was designed for showing all details about Customer and Full Address Details
+"""
 #Customer Routers
 router.register("customers", CustomerViewSet)
 router.register("addresses", AddressViewSet)
 router.register("cities", CityViewSet)
 router.register("countries", CountryViewSet)
 router.register("addresses-detailed", AddressDetailedViewSet)
+
+"""
+    ## these routers was designed for showing all details about Basket and Basket Items
+"""
+#Basket Routers
+router.register("baskets", BasketViewSet)
+router.register("basketitems", BasketItemViewSet)
+router.register("basketitems-detailed", BasketItemDetailedViewSet)
 
 urlpatterns = [
     path("api/", include(router.urls)),
