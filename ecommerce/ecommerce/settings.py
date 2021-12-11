50401b3d4a0242b624429b37cc728e4abe46a327
Shathra <selcuk.gulcan@bilkent.edu.tr>
2021-12-11 20:40:01 +0300

    added pagination for rest endpoints

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -152,3 +152,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = "customers.Customer"
 
 LOCALE_PATHS = (str(BASE_DIR / "locale/"), )
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 2
+}
