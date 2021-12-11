88de4ca431d4a0ed5f5b4398be752b715e4286c0
Oz Onuk <oguzonuk+1@gmail.com>
2021-12-11 11:40:32 +0000

    For payments app: serializers added,filters added, viewsets added to views

--- ecommerce/payments/views.py
+++ ecommerce/payments/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from payments.filters import BankFilter, BankAccountFilter
+from payments.models import Bank, BankAccount
+from payments.serializers import BankSerializer, BankAccountSerializer, BankAccountDetailedSerializer
+
+
+class BankViewSet(viewsets.ModelViewSet):
+    queryset = Bank.objects.all()
+    serializer_class = BankSerializer
+    filterset_class = BankFilter
+
+
+class BankAccountViewSet(viewsets.ModelViewSet):
+    queryset = BankAccount.objects.all()
+    serializer_class = BankAccountSerializer
+    filterset_class = BankAccountFilter
+    serializer_action_classes = {
+        "detailed_list": BankAccountDetailedSerializer,
+        "detailed": BankAccountDetailedSerializer,
+    }
