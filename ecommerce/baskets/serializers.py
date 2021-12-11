486bc111d3383cf70138afd8948c0439ecb91d37
Oz Onuk <oguzonuk+1@gmail.com>
2021-12-11 11:37:30 +0000

    For Baskets app: serializers added,filters added, viewsets added to views

---
+++ ecommerce/baskets/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+
+from baskets.models import Basket, BasketItem
+from customers.serializers import CustomerSerializer
+from products.serializers import ProductSerializer
+
+
+class BasketSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Basket
+        fields = ('id', 'customer', 'status')
+
+
+class BasketDetailedSerializer(BasketSerializer):
+    customers = CustomerSerializer()
+
+
+class BasketItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BasketItem
+        fields = ('id', 'basket', 'product', 'quantity', 'price')
+
+
+class BasketItemDetailedSerializer(BasketItemSerializer):
+    baskets = BasketSerializer()
+    products = ProductSerializer()
+
