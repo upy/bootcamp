db0e2ee23d02c9403514ce4c15d698f29b24a6c2
Melih <melih3553@gmail.com>
2021-12-12 04:05:56 +0300

    filters.py and serializers.py created, views.py and urls.py modified accordingly in payments app

---
+++ ecommerce/payments/serializers.py
@@ -0,0 +1,21 @@
+
+from rest_framework import serializers
+
+from payments.models import Bank, BankAccount
+
+
+class BankSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bank
+        fields = ("id", "name",)
+
+
+class BankAccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BankAccount
+        fields = ("id", "bank", "name", "iban",)
+
+
+class BankAccountDetailedSerializer(BankAccountSerializer):
+    bank = BankSerializer()
+
