b38aaa96745555f96a9e0c72d00725cd9816a0d4
AydinAlptug <yusufaydinceng@gmail.com>
2021-12-12 12:20:40 +0300

    added multiple serializers mixin

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -17,10 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 from ecommerce.router import router
-from products.views import ProductViewSet, CategoryViewSet, ProductDetailedViewSet
+from products.views import ProductViewSet, CategoryViewSet
 
 router.register("products", ProductViewSet)
-router.register("products-detailed", ProductDetailedViewSet, basename="product-detailed")
 router.register("categories", CategoryViewSet)
 
 urlpatterns = [
--- ecommerce/products/managers.py
+++ ecommerce/products/managers.py
@@ -7,3 +7,9 @@ class ProductQuerySet(models.QuerySet):
 
     def has_stock(self):
         return self.filter(stock__quantity__gt=0)
+
+    def action_detailed_list(self):
+        return self.prefetch_related("categories")
+
+    def action_detailed(self):
+        return self.prefetch_related("categories")
