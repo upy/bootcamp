e677a752a62fde94ff3907ea45a0743cb604e54e
Beyza Koroglu <beyza.koroglu.97@gmail.com>
2021-12-18 19:04:57 +0300

    added add_product action

--- ecommerce/baskets/views.py
+++ ecommerce/baskets/views.py
@@ -1,8 +1,11 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from baskets.filters import BasketItemFilter, BasketFilter
 from baskets.models import BasketItem, Basket
-from baskets.serializers import BasketItemSerializer, BasketSerializer, BasketItemDetailedSerializer, BasketDetailedSerializer
+from baskets.serializers import BasketItemSerializer, BasketSerializer, BasketItemDetailedSerializer, \
+    BasketDetailedSerializer
 from core.mixins import DetailedViewSetMixin
 
 
@@ -13,7 +16,22 @@ class BasketItemViewSet(DetailedViewSetMixin, viewsets.ModelViewSet):
     serializer_action_classes = {
         "detailed_list": BasketItemDetailedSerializer,
         "detailed": BasketItemDetailedSerializer,
+        "add_product": BasketItemDetailedSerializer,
     }
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        user = self.request.user
+        return queryset.filter(basket__customer=user)
+
+    @action(detail=True, methods=['post'], http_method_names=['post'])
+    def add_product(self, request):
+        serializer = BasketItemSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class BasketViewSet(DetailedViewSetMixin, viewsets.ModelViewSet):
@@ -24,3 +42,8 @@ class BasketViewSet(DetailedViewSetMixin, viewsets.ModelViewSet):
         "detailed_list": BasketDetailedSerializer,
         "detailed": BasketDetailedSerializer,
     }
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        user = self.request.user
+        return queryset.filter(customer=user)
