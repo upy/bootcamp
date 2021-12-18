2d2229fb942d028e8c25231b3fc3ca399ec3f688
smlblr <samilblr@gmail.com>
2021-12-18 06:48:43 +0300

    Removed the requirement to be authenticated on the start screen that /api/

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -54,9 +54,9 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
+    # 'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.IsAuthenticated',
+    # ],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
