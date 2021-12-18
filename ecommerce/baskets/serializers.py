9b0a705a1000aa3a24ba935cb1bbd923b0aafc4f
Tahakbas <tahanumanakbas@gmail.com>
2021-12-18 18:25:35 +0300

    added "add_to_create" action

--- ecommerce/baskets/serializers.py
+++ ecommerce/baskets/serializers.py
@@ -1,22 +1,25 @@
+from django.db.transaction import atomic
+from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 
 from baskets.models import BasketItem, Basket
 from customers.serializers import CustomerSerializer
-from products.serializers import ProductSerializer
+from products.models import Product
+from products.serializers import ProductSerializer,ProductDetailedSerializer
 
 
 class BasketItemSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = BasketItem
-        fields = ("id", "basket", "product", "quantity", "price")
+        fields = ("id", "basket", "customer", "product", "quantity", "price")
 
 
 class BasketSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Basket
-        fields = ("id", "customer", "status")
+        fields = ("id", "customer","status")
 
 
 class BasketItemDetailedSerializer(BasketItemSerializer):
@@ -26,3 +29,12 @@ class BasketItemDetailedSerializer(BasketItemSerializer):
 
 class BasketDetailedSerializer(BasketSerializer):
     customer = CustomerSerializer()
+
+
+class AddToBasketSerializer(serializers.ModelSerializer):
+      product = ProductDetailedSerializer()
+    #basketItem = BasketItemSerializer()
+
+      class Meta:
+        model = BasketItem
+        fields = ("id", "basket", "customer","product", "quantity", "price")
