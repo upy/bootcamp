b293c017744c7cfef8abe1a5f2504dd7a6430198
ogzhncvdr <oguzhancavdar06@gmail.com>
2021-12-18 16:51:28 +0300

    create basket create endpoint

--- ecommerce/baskets/views.py
+++ ecommerce/baskets/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from baskets.filters import BasketItemFilter, BasketFilter
 from baskets.models import BasketItem, Basket
@@ -7,6 +9,7 @@ from core.mixins import DetailedViewSetMixin
 
 
 class BasketItemViewSet(DetailedViewSetMixin, viewsets.ModelViewSet):
+    permission_classes = ()
     queryset = BasketItem.objects.all()
     serializer_class = BasketItemSerializer
     filterset_class = BasketItemFilter
@@ -15,12 +18,43 @@ class BasketItemViewSet(DetailedViewSetMixin, viewsets.ModelViewSet):
         "detailed": BasketItemDetailedSerializer,
     }
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        user = self.request.user.id
+        return queryset.filter(basket__customer__id=user)
+
 
 class BasketViewSet(DetailedViewSetMixin, viewsets.ModelViewSet):
+    permission_classes = ()
     queryset = Basket.objects.all()
     serializer_class = BasketSerializer
     filterset_class = BasketFilter
     serializer_action_classes = {
         "detailed_list": BasketDetailedSerializer,
         "detailed": BasketDetailedSerializer,
+        "add_basket_item": BasketItemSerializer,
+        "get_basket_items": BasketItemSerializer,
     }
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        user = self.request.user
+        return queryset.filter(customer=user)
+
+    @action(detail=False, methods=['get'], http_method_names=['get'])
+    def get_basket_items(self, request):
+        user = self.request.user
+        basket_items_list = self.get_queryset().filter(
+            customer__id=user.id).first().basketitem_set.all()
+        serializer = self.get_serializer(basket_items_list, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @action(detail=False, methods=["post", "options", "get"])
+    def add_basket_item(self, request):
+        serializer = BasketItemSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
