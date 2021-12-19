a2316f147b72e5d7279f8f6497d89ee37027728a
Melih <melih3553@gmail.com>
2021-12-19 03:27:18 +0300

    added add_product in baskets

--- ecommerce/baskets/views.py
+++ ecommerce/baskets/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 from baskets.filters import BasketItemFilter, BasketFilter
 from baskets.models import BasketItem, Basket
@@ -6,7 +8,8 @@ from baskets.serializers import BasketItemSerializer, BasketSerializer, BasketIt
 from core.mixins import DetailedViewSetMixin
 
 
-class BasketItemViewSet(DetailedViewSetMixin, viewsets.ModelViewSet):
+class BasketItemViewSet(DetailedViewSetMixin,viewsets.ModelViewSet):
+    http_method_names = ["get"]
     queryset = BasketItem.objects.all()
     serializer_class = BasketItemSerializer
     filterset_class = BasketItemFilter
@@ -15,12 +18,35 @@ class BasketItemViewSet(DetailedViewSetMixin, viewsets.ModelViewSet):
         "detailed": BasketItemDetailedSerializer,
     }
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        user = self.request.user.id
+        return queryset.filter(basket__customer__id=user)
+
 
 class BasketViewSet(DetailedViewSetMixin, viewsets.ModelViewSet):
+    permission_classes = ()
+    http_method_names = ["get", "delete", "post"]
     queryset = Basket.objects.all()
     serializer_class = BasketSerializer
     filterset_class = BasketFilter
     serializer_action_classes = {
         "detailed_list": BasketDetailedSerializer,
         "detailed": BasketDetailedSerializer,
+        "create_product": BasketItemSerializer
     }
+
+    def get_queryset(self):
+        # customer can only see own basket
+        queryset = super().get_queryset()
+        user = self.request.user
+        return queryset.filter(customer=user)
+
+    @action(detail=True, methods=['post'])
+    def create_product(self, request, pk=None):
+        serializer = BasketItemSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
